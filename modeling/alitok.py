ac32c43b5229b26b5e4162883acb295338fcefc0
wpy1999 <95274049+wpy1999@users.noreply.github.com>
2025-06-20 20:08:03 +0800

    Update alitok.py

--- modeling/alitok.py
+++ modeling/alitok.py
@@ -138,24 +138,6 @@ class Attention(nn.Module):
         x = self.proj_drop(x)
         return x
 
-class Mlp(nn.Module):
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
-        super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act = act_layer()
-        self.fc2 = nn.Linear(hidden_features, out_features)
-        self.drop = nn.Dropout(drop)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.act(x)
-        x = self.drop(x)
-        x = self.fc2(x)
-        x = self.drop(x)
-        return x
-
 def _expand_token(token, batch_size: int):
     return token.unsqueeze(0).expand(batch_size, -1, -1)
 
@@ -280,18 +262,8 @@ class Decoder(nn.Module):
 class VAutoencoder(nn.Module): 
     def __init__(
         self,
-        dim=128,
-        z_dim=4,
-        dim_mult=[1, 2, 4, 4],
-        num_res_blocks=2,
-        attn_scales=[], 
     ):
         super().__init__()
-        self.dim = dim 
-        self.z_dim = z_dim
-        self.dim_mult = dim_mult
-        self.num_res_blocks = num_res_blocks
-        self.attn_scales = attn_scales 
 
         # modules
         self.encoder = Encoder()
